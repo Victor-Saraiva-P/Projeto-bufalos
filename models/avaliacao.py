0c17adee4ab2040c53c7cb904860e87ce586a1f9
Victor Alexandre Saraiva Pimentel <victoralex07062005@gmail.com>
2026-02-02 16:51:58 -0300

    instancia as avaliacoes e calcula a area do ground of truth

---
+++ models/avaliacao.py
@@ -0,0 +1,26 @@
+import os.path
+
+from models.mascara import Mascara
+
+
+class Avaliacao:
+    nome_arquivo: str
+    ground_truth: Mascara
+    segmetacoes: list[Mascara]
+
+    def __init__(
+            self,
+            nome_arquivo: str,
+            mascaras_type: str,
+            ground_truth_dir: str,
+            # segmetacoes_dir: str,
+            # segmetacoes_modelos: list[str],
+    ):
+        self.nome_arquivo = nome_arquivo
+
+        ground_truth_path = os.path.join(ground_truth_dir, f"{nome_arquivo}{mascaras_type}")
+        self.ground_truth = Mascara("ground-of-truth", ground_truth_path)
+
+        # self.segmetacoes = [
+        #     Mascara(nome_arquivo, modelo) for modelo in segmetacoes_modelos
+        # ]
--- models/mascara.py
+++ models/mascara.py
@@ -1,8 +1,27 @@
-from dataclasses import dataclass
-from decimal import Decimal
+import numpy as np
+from PIL import Image
 
 
-@dataclass(frozen=True)
 class Mascara:
+    modelo: str
     area: int
-    perimetro: int
+
+    def __init__(self, modelo: str, image_path: str):
+        self.modelo = modelo
+        self.area = self.calcular_area(image_path)
+
+        # self._perimetro = self.calcular_perimetro()
+
+    @staticmethod
+    def calcular_area(image_path: str):
+        print("Calculando area")
+        with Image.open(image_path) as img:
+            img_gray = img.convert("L")
+            arr = np.array(img_gray)
+
+        area = int((arr == 255).sum())
+
+        return area
+
+    # def calcular_perimetro(self, image_path: str):
+    #     return 0
